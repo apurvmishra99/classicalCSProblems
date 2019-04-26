9b0920857410adce3545b4ba2c69c0f3781a2b7b
Apurv Mishra <apurvmishra99@gmail.com>
2019-04-27 00:48:51 +0100

    complete fibonacci

--- smallProblems/fibonacci/fibonacciSeries.py
+++ smallProblems/fibonacci/fibonacciSeries.py
@@ -11,6 +11,6 @@ def fibo_series(n: int):
         last, next = next, last + next
         yield next # generation step
 
-    if __name__ == "__main__":
-        for i in fibo_series(51):
-            print(i)
+if __name__ == "__main__":
+    for i in fibo_series(50):
+        print(i)
